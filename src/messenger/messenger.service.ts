f37d3d058957b020da56e7bafafe32f3cd51baf1
Ibrahim Suleiman <suleimanonnet@gmail.com>
2026-04-13 10:42:22 +0100

    chore: enable create message to send email

--- src/messenger/messenger.service.ts
+++ src/messenger/messenger.service.ts
@@ -123,13 +123,13 @@ export class MessengerService {
 
       const emailTemplate = EMAIL_TEMPLATES.MESSAGE_TO_FUTURE;
 
-      // this.emailService.send({
-      //   recipientEmail,
-      //   recipientName,
-      //   emailSubject,
-      //   emailData,
-      //   emailTemplate,
-      // });
+      this.emailService.send({
+        recipientEmail,
+        recipientName,
+        emailSubject,
+        emailData,
+        emailTemplate,
+      });
 
       message.sent = true;
       this.messageToFutureRepository.save(message);
