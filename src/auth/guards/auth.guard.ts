9e40b58189bc7b82219005a55272ce2847e2d9f2
Ibrahim Suleiman <suleimanonnet@gmail.com>
2026-05-11 10:53:45 +0100

    chore: update error message

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -29,7 +29,7 @@ export class AuthGuard implements CanActivate {
 
     if (!bearerToken)
       throw new UnauthorizedException(
-        'please provide Bearer token in Authorization header.',
+        'Please provide Bearer token in Authorization header.',
       );
 
     const token = bearerToken.split(' ')[1];
